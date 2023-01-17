cdca663fb2621923a110cce380e88f3b93986325
Md. Yeamin Islam <yeamin15-13529@diu.edu.bd>
2023-01-17 07:53:57 +0600

    Udemy_python_excercise files added

---
+++ Udemy_Python_excercise_300+/1st_Problem/problem_yeamin_solution.py
@@ -0,0 +1,5 @@
+print("Printing the result:")
+print("Number \t\t Square \t Cube")
+Mylist = [1,2,3,4,5]
+for i in Mylist:
+    print(i, "\t\t", i*i,"\t\t",i**3)
---
+++ Udemy_Python_excercise_300+/2nd_Problem/problem_circle_yeamin.py
@@ -0,0 +1,4 @@
+#find out the circle area
+radius = int(input("Enter the radius of the circle: "))
+result = 3.14159 * radius * radius
+print("The area of the circle is : ",result)
